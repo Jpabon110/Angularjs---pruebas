7592ef9cb880df7f180dccdf67b5cc60bcc9a223
Juan J Pabon T <jpabon110@gmail.com>
2017-01-11 12:03:38 -0400

    Nueva carpeta

--- rene/lista.js
+++
@@ -1,9 +0,0 @@
-angular.module("list", ["LocalStorageModule"])
-.controller("Tlist", ["$scope","$localStorageService", function(s,l){
-	s.lista = [];
-	s.nombre = "rene";
-	s.agregarLista = function(){
-		s.lista.push(s.nuevaLista);
-		s.nuevaLista = {};
-	}
-}]);
